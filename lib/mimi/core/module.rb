ab7ca0febea8b27e3adc9e1b87a001f59462971f
Alex Kukushkin <alex.kukushkin@strongkeep.com>
2018-09-10 20:01:41 +0200

    Refactored Mimi::Core::Module to utilize manifests, updated doc comments

--- lib/mimi/core/module.rb
+++ lib/mimi/core/module.rb
@@ -14,49 +14,95 @@ module Mimi
       end
 
       module ClassMethods
+        #
+        # Processes given values for configurable parameters defined in the module manifest
+        # and populates the options Hash.
+        #
+        # @param opts [Hash] values for configurable parameters
+        #
         def configure(opts = {})
-          @options = {}.deep_merge(opts)
+          manifest_hash = manifest
+          unless manifest_hash.is_a?(Hash)
+            raise "#{self}.manifest should be implemented and return Hash"
+          end
+          @options = Mimi::Core::Manifest.new(manifest_hash).apply(opts)
         end
 
+        # Returns the path to module files, if the module exposes any files.
+        #
+        # Some modules may expose its files to the application using Mimi core. For example,
+        # a module may contain some rake tasks with useful functionality.
+        #
+        # To expose module files, this method must be overloaded and point to the root
+        # of the gem folder:
+        #
+        # ```
+        # # For example, module my_lib folder and files:
+        # /path/to/my_lib/
+        #   ./lib/my_lib/...
+        #   ./lib/my_lib.rb
+        #   ./spec/spec_helper
+        #   ...
+        #
+        # # my_lib module should expose its root as .module_path: /path/to/my_lib
+        # ```
+        #
+        # @return [Pathname,String,nil]
+        #
         def module_path
           nil
         end
 
+        # Module manifest
+        #
+        # Mimi modules overload this method to define their own set of configurable parameters.
+        # The method should return a Hash representation of the manifest.
+        #
+        # NOTE: to avoid clashes with other modules, it is advised that configurable parameters
+        # for the module have some module-specific prefix. E.g. `Mimi::DB` module has its
+        # configurable parameters names as `db_adapter`, `db_database`, `db_username` and so on.
+        #
+        # @see Mimi::Core::Manifest
+        #
+        # @return [Hash]
+        #
         def manifest
-          {
-            my_var1: {
-              default: 123,
-              type: :integer,
-              desc: 'My Variable 1'
-            },
-
-            my_var2: {
-              type: :string
-            },
-
-            my_var3: {
-              desc: 'Description of the var 3',
-              type: [:string, :integer, :decimal, :boolean, :json, :enum],
-              hidden: true
-            }
-          }
-
-          #
           {}
         end
 
+        # Starts the module.
+        #
+        # Mimi modules overload this method to implement some module-specific logic that
+        # should happen on application startup. E.g. `mimi-messaging` establishes a connection
+        # with a message broker and declares message consumers.
+        #
         def start(*)
           @module_started = true
         end
 
+        # Returns true if the module is started.
+        #
+        # @return [true,false]
+        #
         def started?
           @module_started
         end
 
+        # Starts the module.
+        #
+        # Mimi modules overload this method to implement some module-specific logic that
+        # should happen on application shutdown. E.g. `mimi-messaging` closes a connection
+        # with a message broker.
+        #
         def stop(*)
           @module_started = false
         end
 
+        # Returns a Hash of configurable parameter values accepted and set by the `.configure`
+        # method.
+        #
+        # @return [Hash<Symbol,Object>]
+        #
         def options
           @options || {}
         end
